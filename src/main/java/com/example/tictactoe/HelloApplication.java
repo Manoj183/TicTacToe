05e300740f99df8162462b55cb56bd4c4067aece
Manoj183 <manojchari2000@gmail.com>
2024-03-14 22:48:52 +0530

    TicTacToe commit

---
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -0,0 +1,202 @@
+package com.example.tictactoe;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    private Label playerXScoreLabel, playerOScoreLabel;
+    private Button buttons[][] = new Button[3][3];
+    private boolean playerXTurn = true;
+    private int playerXScore = 0, playerOScore = 0;
+
+
+    private BorderPane createContent(){
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(20));
+        root.setStyle("-fx-background-color: #FDE74C;");
+
+        // Title
+        Label titleLabel = new Label("Tic Tac Toe");
+        titleLabel.setStyle("-fx-font-size: 24pt; -fx-font-weight: bold;-fx-text-fill:black;");
+        titleLabel.setPadding(new Insets(0, 0, 10, 0));
+        root.setTop(titleLabel);
+        BorderPane.setAlignment(titleLabel, Pos.CENTER);
+
+        //Game board
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setAlignment(Pos.CENTER);
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                Button button = new Button();
+                button.setPrefSize(100,100);
+                button.setStyle(
+                        "-fx-font-size: 30pt; " +
+                        "-fx-font-weight: bold;" +
+                        "-fx-border-color: #5BC0EB;"+
+                        "-fx-border-width: 2px;"+
+                        "-fx-background-color: #1be7ff;");
+                button.setOnAction(event->buttonClicked(button));
+                buttons[i][j] = button;
+                gridPane.add(button,j,i);
+                DropShadow shadow = new DropShadow();
+                shadow.setRadius(5);
+                button.setOnMousePressed(e -> button.setEffect(shadow));
+                button.setOnMouseReleased(e -> button.setEffect(null));
+            }
+        }
+        root.setCenter(gridPane);
+        BorderPane.setAlignment(gridPane, Pos.CENTER);
+
+        //Score board
+        HBox scoreBoard = new HBox(20);
+        scoreBoard.setPadding(new Insets(10, 0, 0, 0));
+        scoreBoard.setAlignment(Pos.CENTER);
+        playerXScoreLabel = new Label("Player X : 0");
+        playerXScoreLabel.setStyle("-fx-font-size: 16pt; -fx-font-weight: bold;-fx-text-fill:black;");
+        playerOScoreLabel = new Label("Player O : 0");
+        playerOScoreLabel.setStyle("-fx-font-size: 16pt; -fx-font-weight: bold;-fx-text-fill:black;");
+        scoreBoard.getChildren().addAll(playerXScoreLabel, playerOScoreLabel);
+        root.setBottom(scoreBoard);
+
+        return root;
+    }
+
+    private void buttonClicked(Button button){
+        if(button.getText().equals("")) {
+            if (playerXTurn) {
+                button.setText("X");
+                button.setTextFill(Color.web("#e71d36"));
+            } else {
+                button.setText("O");
+                button.setTextFill(Color.web("#5603ad"));
+            }
+            playerXTurn = !playerXTurn;
+            checkWinner();
+        }
+    }
+    private void checkWinner(){
+        for(int row = 0; row < 3; row++){
+            if(     buttons[row][0].getText().equals(buttons[row][1].getText()) &&
+                    buttons[row][1].getText().equals(buttons[row][2].getText()) &&
+                    !buttons[row][0].getText().isEmpty()
+              ){
+                String winner = buttons[row][0].getText();
+                showWinnerDialog(winner);
+                updateScore(winner);
+                resetBoard();
+                return;
+            }
+        }
+        for(int col = 0; col < 3; col++){
+            if(     buttons[0][col].getText().equals(buttons[1][col].getText()) &&
+                    buttons[1][col].getText().equals(buttons[2][col].getText()) &&
+                    !buttons[0][col].getText().isEmpty()
+            ){
+                String winner = buttons[0][col].getText();
+                showWinnerDialog(winner);
+                updateScore(winner);
+                resetBoard();
+                return;
+            }
+        }
+        if(     buttons[0][0].getText().equals(buttons[1][1].getText()) &&
+                buttons[1][1].getText().equals(buttons[2][2].getText()) &&
+                !buttons[0][0].getText().isEmpty()
+        ){
+            String winner = buttons[0][0].getText();
+            showWinnerDialog(winner);
+            updateScore(winner);
+            resetBoard();
+            return;
+
+        }
+        if(     buttons[2][0].getText().equals(buttons[1][1].getText()) &&
+                buttons[1][1].getText().equals(buttons[0][2].getText()) &&
+                !buttons[2][0].getText().isEmpty()
+        ){
+            String winner = buttons[2][0].getText();
+            showWinnerDialog(winner);
+            updateScore(winner);
+            resetBoard();
+            return;
+        }
+
+        boolean tie = true;
+        for(Button row[] : buttons){
+            for(Button button : row){
+                if(button.getText().isEmpty()){
+                    tie = false;
+                    break;
+                }
+            }
+        }
+        if(tie){
+            showTieDialog();
+            resetBoard();
+        }
+    }
+    private void showWinnerDialog(String winner){
+       Alert alert = new Alert(Alert.AlertType.INFORMATION);
+       alert.setTitle("Winner");
+       alert.setContentText("Congratulations "+ winner +"! You won the game");
+       alert.setHeaderText("");
+       alert.showAndWait();
+    }
+    private void showTieDialog(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Winner");
+        alert.setContentText("Game Over ! It's a tie");
+        alert.setHeaderText("");
+        alert.showAndWait();
+    }
+
+    private void updateScore(String winner){
+        if(winner.equals("X")) {
+            playerXScore++;
+            playerXScoreLabel.setText("Player X : " + playerXScore);
+        }
+        else{
+            playerOScore++;
+            playerOScoreLabel.setText("Player O : " + playerOScore);
+        }
+    }
+
+    private void resetBoard(){
+        for(Button row[] : buttons){
+            for(Button button : row){
+                button.setText("");
+            }
+        }
+    }
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Scene scene = new Scene(createContent());
+        stage.setTitle("TicTacToe");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
+
