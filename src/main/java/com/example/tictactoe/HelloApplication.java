d40af4e083796afe7ac121085ed4248560c80442
Manoj183 <manojchari2000@gmail.com>
2024-03-14 22:56:41 +0530

    TicTacToe commit

--- src/main/java/com/example/tictactoe/HelloApplication.java
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -14,8 +14,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
 
     private Label playerXScoreLabel, playerOScoreLabel;
